b9250c4b3e5b76dd928458511a255f3883a716e1
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-12-31 15:00:38 -0500

    Feat: add CalendarHelperController with week_starting action and corresponding route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get "calendar_helper/week_starting"
       resources :accounts, only: %i[create show update destroy]
       resources :sessions, only: %i[index show create update destroy]
     end
