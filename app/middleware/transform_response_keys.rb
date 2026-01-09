253d1b5acc55d94a6f4313f1a3275cf1c527af09
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2026-01-09 09:22:39 -0500

    Fix Rubocop problems

--- app/middleware/transform_response_keys.rb
+++ app/middleware/transform_response_keys.rb
@@ -5,81 +5,102 @@ class TransformResponseKeys
 
   def call(env)
     status, headers, response = @app.call(env)
+    if json_content?(headers)
+      body = extract_body(response)
+      return [status, headers, response] if body.nil? || body.strip.empty?
 
-    if headers["Content-Type"]&.include?("application/json")
-      body = ""
-      response.each { |part| body << part }
+      parsed = parse_json(body)
+      return [status, headers, response] unless parsed
 
-      # If there's no body, or it's not valid JSON, skip transformation.
-      begin
-        return [status, headers, response] if body.nil? || body.strip.empty?
+      transformed_body = transform_body_keys(parsed)
 
-        parsed_body = JSON.parse(body)
-      rescue JSON::ParserError
-        return [status, headers, response]
-      end
+      transform_header_urls(headers)
 
-      transformed_body = KeyTransformer.deep_transform_keys(parsed_body) do |key|
-        KeyTransformer.camelize(key)
-      end
-
-      # Also transform query params present in common header URLs (Location, Content-Location)
-      begin
-        require "uri"
-        require "rack"
-
-        ["Location", "Content-Location"].each do |h|
-          next unless headers[h]
-          begin
-            uri = URI.parse(headers[h]) rescue nil
-            next unless uri && uri.query && !uri.query.empty?
-            qhash = Rack::Utils.parse_nested_query(uri.query) rescue nil
-            if qhash.is_a?(Hash)
-              new_q = Rack::Utils.build_nested_query(
-                KeyTransformer.deep_transform_keys(qhash) { |k| KeyTransformer.camelize(k) }
-              )
-              uri.query = new_q
-                headers[h] = "#{uri}"
-            end
-          rescue => _e
-            # ignore and leave header as-is
-          end
-        end
-
-        # Transform any URLs inside a Link header
-        if headers["Link"]
-          headers["Link"] = headers["Link"].gsub(/<([^>]+)>/) do |m|
-            url = $1
-            begin
-              uri = URI.parse(url) rescue nil
-              if uri && uri.query && !uri.query.empty?
-                qhash = Rack::Utils.parse_nested_query(uri.query) rescue nil
-                if qhash.is_a?(Hash)
-                  new_q = Rack::Utils.build_nested_query(
-                    KeyTransformer.deep_transform_keys(qhash) { |k| KeyTransformer.camelize(k) }
-                  )
-                  uri.query = new_q
-                  "<#{uri}>"
-                else
-                  m
-                end
-              else
-                m
-              end
-            rescue => _e
-              m
-            end
-          end
-        end
-      rescue => _e
-        # ignore header-query transformation failures
-      end
-
-      new_body = [transformed_body.to_json]
+      new_body = build_response_body(transformed_body)
       headers["Content-Length"] = new_body.sum(&:bytesize).to_s
       [status, headers, new_body]
     else
       [status, headers, response]
+    end
+  end
+
+  private
+
+  def json_content?(headers)
+    headers["Content-Type"]&.include?("application/json")
+  end
+
+  def extract_body(response)
+    body = ""
+    response.each { |part| body << part }
+    body
+  end
+
+  def parse_json(body)
+    JSON.parse(body)
+  rescue JSON::ParserError
+    nil
+  end
+
+  def transform_body_keys(parsed)
+    KeyTransformer.deep_transform_keys(parsed) do |key|
+      KeyTransformer.camelize(key)
+    end
+  end
+
+  def build_response_body(transformed_body)
+    [transformed_body.to_json]
+  end
+
+  def transform_header_urls(headers)
+    require "uri"
+    require "rack"
+
+    transform_location_headers(headers)
+    transform_link_header(headers)
+  rescue => _e
+    # ignore header-query transformation failures
+  end
+
+  def transform_location_headers(headers)
+    ["Location", "Content-Location"].each do |h|
+      next unless headers[h]
+      begin
+        uri = URI.parse(headers[h]) rescue nil
+        next unless uri && uri.query && !uri.query.empty?
+        qhash = Rack::Utils.parse_nested_query(uri.query) rescue nil
+        if qhash.is_a?(Hash)
+          new_q = Rack::Utils.build_nested_query(
+            KeyTransformer.deep_transform_keys(qhash) { |k| KeyTransformer.camelize(k) }
+          )
+          uri.query = new_q
+          headers[h] = "#{uri}"
+        end
+      rescue => _e
+        # ignore and leave header as-is
+      end
+    end
+  end
+
+  def transform_link_header(headers)
+    return unless headers["Link"]
+    headers["Link"] = headers["Link"].gsub(/<([^>]+)>/) do |m|
+      url = $1
+      begin
+        uri = URI.parse(url)
+        next m unless uri&.query&.length.to_i > 0
+
+        qhash = Rack::Utils.parse_nested_query(uri.query)
+        next m unless qhash.is_a?(Hash) && qhash.any?
+
+        uri.query = Rack::Utils.build_nested_query(
+          KeyTransformer.deep_transform_keys(qhash) { |k| KeyTransformer.camelize(k) }
+        )
+
+        "<#{uri}>"
+      rescue StandardError
+        m
+      end
     end
   end
 end
